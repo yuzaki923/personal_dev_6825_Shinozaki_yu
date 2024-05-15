05357265d24cd1f2130cebd90872f11d00aee180
yuzaki923 <yu.shinozaki923@gmail.com>
2024-05-15 14:56:42 +0900

    ファーストコミット

---
+++ src/main/java/com/example/demo/PersonalDev6825ShinozakiYuApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalDev6825ShinozakiYuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalDev6825ShinozakiYuApplication.class, args);
+	}
+
+}
---
+++ src/main/resources/schema.sql
@@ -0,0 +1,48 @@
+-- 各種テーブル削除
+DROP TABLE IF EXISTS categories;
+DROP TABLE IF EXISTS items;
+DROP TABLE IF EXISTS customers;
+DROP TABLE IF EXISTS orders;
+DROP TABLE IF EXISTS order_details;
+
+-- カテゴリーテーブル
+CREATE TABLE categories
+(
+   id SERIAL PRIMARY KEY,
+   name TEXT
+);
+-- 商品テーブル
+CREATE TABLE items
+(
+   id SERIAL PRIMARY KEY,
+   category_id INTEGER,
+   name TEXT,
+   price INTEGER
+);
+-- 顧客テーブル
+CREATE TABLE customers
+(
+   id SERIAL PRIMARY KEY,
+   name TEXT,
+   address TEXT,
+   tel TEXT,
+   email TEXT,
+   password TEXT
+);
+-- 注文テーブル
+CREATE TABLE orders
+(
+   id SERIAL PRIMARY KEY,
+   customer_id INTEGER,
+   ordered_on DATE,
+   total_price INTEGER
+   
+);
+-- 注文明細テーブル
+CREATE TABLE order_details
+(
+   id SERIAL PRIMARY KEY,
+   order_id INTEGER,
+   item_id INTEGER,
+   quantity INTEGER
+);
